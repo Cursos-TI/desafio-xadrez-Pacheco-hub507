be7c782345b51f9f86235d3267759b29ee9ea6aa
Pacheco-hub507 <edigleipacheco@gmail.com>
2025-10-31 13:58:36 +0000

    Movimeto do Cavalo

--- xadrez.c
+++ xadrez.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    // Mover A Torre 5 casas para a direita
+    // Move a Torre 5 casas para a direita
     for (int i = 0; i < 5; i++) {
         printf("Direita\n"); // Imprime a direção do movimento
     }
+
+    printf("\n");
 
     // Move o Bispo 5 casas diagonais à direita
     int contador = 0;
@@ -13,6 +15,8 @@ int main() {
         printf("Cima Direita\n", contador); // Imprime a direção do movimento
         contador++;
     }
+
+    printf("\n");
 
     // Move a Rainha 8 casas à esquerda
     int movimento = 1;
@@ -23,5 +27,19 @@ int main() {
 
     } while (movimento <= 8);
 
+    printf("\n");
+
+    // Move o cavalo 2 casas para baixo e 1 casa para a esquerda
+    int movimentoClompleto = 1; // Flag para controlar o movimeto "L"
+
+    while (movimentoClompleto--)
+    {
+       for (int i = 0; i <3; i++) {
+           printf("Baixo\n"); // Imprime "Baixo 3 vezes"
+
+       }
+       printf("Esquerda\n"); // Imprime "Direita" uma vez
+    } 
+       
     return 0;
 }
